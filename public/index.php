e876d586293bf77a936d31f06e1fd76856aebf15
michael <michael.araujo@4linux.com.br>
2016-07-16 13:44:30 -0300

    Ajuste

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use alunos\biblioteca\Files\Csv\Csv;
+
+$csv = new Csv();
---
+++ src/alunos/biblioteca/Files/Csv/Csv.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace alunos\biblioteca\Files\Csv;
+
+/**
+* Class resposavel por realizar um parser no arquivo CSV e importar os dados para o Database
+* @author Michael Douglas
+* @license GNU
+* @copyright mdba_araujo
+* @category Class import files csv to database
+* @version 0.1
+*/
+
+class Csv
+{
+}
